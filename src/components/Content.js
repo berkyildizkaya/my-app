8e9902108cb05fc1772bfe198cd410b269c5e1bd
Berk Yıldızkaya <48087914+berkyildizkaya@users.noreply.github.com>
2023-01-30 23:23:48 +0300

    First Commit

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import {Container} from "react-bootstrap";
+import Slider from "./Slider";
+
+
+function Content(){
+    return (
+        <Container className="mt-3">
+            <Slider></Slider>
+            
+        </Container>
+    );
+}
+export default Content;
---
+++ src/components/mainMenu.js
@@ -0,0 +1,22 @@
+import  Container  from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function MainMenu() {
+    return (
+        <>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#AnaSayfa">Anasayfa</Nav.Link>
+                    <Nav.Link href="#Galeri">Galeri</Nav.Link>
+                    <Nav.Link href="#Contact">İletişim</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        </>
+    );
+}
+
+export default MainMenu;
