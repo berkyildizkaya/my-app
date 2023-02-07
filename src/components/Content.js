95df90fd26d7c550cd7716cf926d0fb4413fdf05
Berk Yıldızkaya <berk.yildizkaya@royrobson.com>
2023-02-07 11:25:15 +0300

    Function Component'e geri dönüldü

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,15 +2,13 @@ import React from "react";
 import {Container} from "react-bootstrap";
 import Slider from "./Slider";
 
-class Content extends React.Component{
-    render() {
-        return (
-            <Container className="mt-3">
-                <Slider></Slider>
-                
-            </Container>
-        );
-    }
+
+function Content () {
+    return (
+        <Container className="mt-3">
+            <Slider></Slider>
+        </Container>
+    )
 }
 
 export default Content;
