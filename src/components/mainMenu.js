a3a1e02055c2911a396e6b0f2a82a57a20cbf51d
Berk Yıldızkaya <berk.yildizkaya@royrobson.com>
2023-02-15 14:00:04 +0300

    Gereksiz Component Silindi ve Router yapısı kuruldu.

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -3,7 +3,7 @@ import  Container  from "react-bootstrap/Container";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import {  Link } from 'react-router-dom';
 function MainMenu(){
     return (
         <>
@@ -13,9 +13,9 @@ function MainMenu(){
                 <Navbar.Toggle aria-controls="responsive-navbar"/>
                 <NavbarCollapse id="responsive-navbar">
                     <Nav className="me-auto">
-                        <Nav.Link href="#AnaSayfa">Anasayfa</Nav.Link>
-                        <Nav.Link href="#Galeri">Galeri</Nav.Link>
-                        <Nav.Link href="#Contact">İletişim</Nav.Link>
+                        <Nav.Link as={Link} to="/">Anasayfa</Nav.Link>
+                        <Nav.Link as={Link} eventKey="/About" to="/About">Hakkımda</Nav.Link>
+                        {/* <Nav.Link as={Link} to="/">İletişim</Nav.Link> */}
                     </Nav>
                 </NavbarCollapse>
             </Container>
